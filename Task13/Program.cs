7c49c163485411a8d6ed23e696f55bbf7e092825
AlexandrKryu <a.kryu84@gmail.com>
2022-08-11 22:36:18 +0300

    ошибка

---
+++ Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число. ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == -100 && num == -1000 || num == 100 && num == 1000)
+{
+    Console.WriteLine($"{num % 10}");
+}
+
+{
+    num / 10;
+}
+Console.WriteLine($"число{num1}");
